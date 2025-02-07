eaa699055152d565dc4ece321f4febf2fd217f78
aayushker <singhaayushker@gmail.com>
2025-02-07 09:48:44 +0530

    chore: Add Tailwind CSS configuration and remove unused CSS files

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+// filepath: /home/gopatron/Documents/WebD Codes/WebTree-Internship-Assignment/frontend/postcss.config.js
+module.exports = {
+    plugins: [
+      'postcss-flexbugs-fixes',
+      'tailwindcss',
+      'autoprefixer',
+    ],
+  };
