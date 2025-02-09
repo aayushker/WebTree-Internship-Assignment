b17481efcceea18f5712f4e14a6e958bab573938
aayushker <singhaayushker@gmail.com>
2025-02-09 15:03:17 +0530

    chore: Remove unused files and configurations from the React app

--- frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// filepath: /home/gopatron/Documents/WebD Codes/WebTree-Internship-Assignment/frontend/postcss.config.js
-module.exports = {
-    plugins: [
-      'postcss-flexbugs-fixes',
-      'tailwindcss',
-      'autoprefixer',
-    ],
-  };
